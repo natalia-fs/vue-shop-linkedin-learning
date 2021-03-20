78db4e8197262b1ca9e6c017a52fd04905fabbdf
Natalia <natalia-321s@outlook.com>
2021-03-20 01:47:46 -0300

    Configurações iniciais

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,9 @@ module.exports = {
     parser: "babel-eslint",
   },
   rules: {
+    "no-multi-spaces": ["error"],
+    "vue/no-multiple-template-root": "off",
+    "no-unsed-vars": "off",
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
